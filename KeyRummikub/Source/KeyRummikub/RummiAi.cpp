d0ef699b634c6f47bb9042a0bd1f76db2eb38c26
KeyruneDeasy <ciarandeasy513@hotmail.com>
2023-02-11 17:39:14 +0200

    Bugfix for the AI setting an invalid board state.

--- KeyRummikub/Source/KeyRummikub/RummiAi.cpp
+++ KeyRummikub/Source/KeyRummikub/RummiAi.cpp
@@ -69,6 +69,10 @@ void URummiAi::DetermineTargetBoardState(AKeyRummikubGameState* GameState)
 						}
 					}
 				}
+				if (bPlayedTile)
+				{
+					break;
+				}
 			}
 			if (bPlayedTile)
 			{
